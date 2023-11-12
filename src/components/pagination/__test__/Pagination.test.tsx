482dec7c78ae17851c1bfd2552d3ebe0be37f275
GEKKO-ops <artem@mikolyuk.com>
2023-11-12 17:33:22 +0200

    feat:add test for 404 page

--- src/components/pagination/__test__/Pagination.test.tsx
+++ src/components/pagination/__test__/Pagination.test.tsx
@@ -52,67 +52,14 @@ describe('Pagination component', () => {
       );
     });
 
-    // Проверка, что компонент корректно отрисовывает количество страниц
-    expect(screen.getAllByRole('link')).toHaveLength(5); // Например, 1, 2, 3, ..., 5
+    expect(screen.getAllByRole('link')).toHaveLength(5);
 
-    // Проверка, что активная страница выделена правильным образом
     expect(screen.getByText('1')).toHaveClass('active');
 
-    // Клик на другую страницу
     fireEvent.click(screen.getByText('3'));
 
-    // Проверка, что обработчик paginate вызван с правильным аргументом
     expect(mockPaginate).toHaveBeenCalledWith(3);
 
-    // Проверка, что активная страница теперь 3
     expect(screen.getByText('3')).toHaveClass('active');
   });
-
-  // test('renders correct page numbers when totalPages > displayLimit', async () => {
-  //   await act(async () => {
-  //     render(
-  //       <MemoryRouter initialEntries={['/search/page/1']}>
-  //         <Routes>
-  //           <Route
-  //             path="/search/page/:page"
-  //             element={
-  //               <Pagination
-  //                 cardPerPage={cardPerPage}
-  //                 totalCard={totalCard}
-  //                 paginate={mockPaginate}
-  //               />
-  //             }
-  //           />
-  //         </Routes>
-  //       </MemoryRouter>
-  //     );
-  //   });
-
-  //   const displayLimit = 10; // Установите значение displayLimit в соответствии с вашей реализацией
-  //   const totalPages = Math.ceil(totalCard / cardPerPage);
-
-  //   fireEvent.click(screen.getByText('1'));
-  //   expect(screen.getAllByRole('link')).toHaveLength(
-  //     Math.min(displayLimit, totalPages)
-  //   );
-  //   expect(screen.getByText('...')).toBeInTheDocument();
-  //   expect(screen.getByText('5')).toBeInTheDocument();
-
-  //   // Проверка, что компонент корректно отображает страницы, когда Number(page) > totalCard! - Math.floor(displayLimit / 2)
-  //   fireEvent.click(screen.getByText('5'));
-  //   expect(screen.getAllByRole('link')).toHaveLength(
-  //     Math.min(displayLimit, totalPages) + 1
-  //   );
-  //   expect(screen.getByText('1')).toBeInTheDocument();
-  //   expect(screen.getByText('...')).toBeInTheDocument();
-  //   expect(screen.getByText('5')).toBeInTheDocument();
-
-  //   // Проверка, что компонент корректно отображает страницы, в остальных случаях
-  //   fireEvent.click(screen.getByText('3'));
-  //   const expectedLength = Math.min(displayLimit, totalPages) + 2;
-  //   expect(screen.getAllByRole('link')).toHaveLength(expectedLength);
-  //   expect(screen.getByText('1')).toBeInTheDocument();
-  //   expect(screen.getByText('...')).toBeInTheDocument();
-  //   expect(screen.getByText('5')).toBeInTheDocument();
-  // });
 });
--- src/pages/page404/NotFoundPage.tsx
+++ src/pages/page404/NotFoundPage.tsx
@@ -1,13 +1,11 @@
 import { FC } from 'react';
-import image from '../../assets/images/page404.png';
 import './notFound.css';
 
 const NotFoundPage: FC = () => {
   return (
     <div className="not-found">
-      <p></p>
       <img
-        src={image}
+        src="../../assets/images/page404.png"
         alt="Not found"
       />
     </div>
